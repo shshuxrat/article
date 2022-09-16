524ea5b2d1eb5ca7fc1e1df5c44d04e7b0356af2
Shoumarov Shuxrat <shoumaorovshuxrat1506@gmail.com>
2022-09-16 12:14:45 +0500

    storage,author interfaces added

--- storage/storage.go
+++ storage/storage.go
@@ -4,11 +4,25 @@ import (
 	"article/models"
 )
 
+type StorageI interface {
+	Article() ArticleRepoI
+	Author() AuthorRepoI
+}
+
 type ArticleRepoI interface {
-	Create(entity models.Article) error
+	Create(entity models.Article) (int64, error)
 	GetList(query models.Query) ([]models.Article, error)
 	GetByID(Id int) (models.Article, error)
 	Search(query models.Query) ([]models.Article, error)
 	Update(entity models.Article) (int64, error)
+	Delete(ID int) (int64, error)
+}
+
+type AuthorRepoI interface {
+	Create(entity models.Person) (int64, error)
+	GetList(query models.Query) ([]models.Person, error)
+	GetByID(Id int) (models.Person, error)
+	Search(query models.Query) ([]models.Person, error)
+	Update(entity models.Person) (int64, error)
 	Delete(ID int) (int64, error)
 }
